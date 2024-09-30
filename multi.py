8261e1ff37bfd802899c00a5722f59477cc86041
Taylor O'Brien <toobrien@amazon.com>
2024-09-30 13:40:42 -0700

    .

--- multi.py
+++ multi.py
@@ -5,16 +5,16 @@ import  plotly.graph_objects    as      go
 from    sys                     import  argv
 
 
-# python 2024-09-30 06:30-14:00 live
+# python 06:30-14:00 live 2024-09-30
 
 
 if __name__ == "__main__":
 
-    date    = argv[1]
-    bounds  = argv[2].split("-")
+    date    = argv[3]
+    bounds  = argv[1].split("-")
     start   = f"{date}T{bounds[0]}"
     end     = f"{date}T{bounds[1]}"
-    mode    = argv[3]
+    mode    = argv[2]
     df      = pl.read_csv(f"./csvs/{mode}/{date}.csv")
     ts      = list(df["ts"])
     i       = bisect_left(ts, start)
@@ -41,5 +41,6 @@ if __name__ == "__main__":
             )
         )
 
+    fig.add_hline(y = 0, line_color = "#FF00FF")
     fig.show()
 
